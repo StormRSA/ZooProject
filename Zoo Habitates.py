f2637eafcf00a8c3c4aa2d87d0429dd118d5f6a8
Storm <optistorm@gmail.com>
2020-04-26 13:11:52 +0200

    Initial commit

---
+++ Zoo Habitates.py
@@ -0,0 +1,157 @@
+camel = """
+------------------------------------------------
+Switching on camera from habitat with camels...
+ ___.-''''-.
+/___  @    |
+',,,,.     |         _.'''''''._
+     '     |        /           \\
+     |     \    _.-'             \\
+     |      '.-'                  '-.
+     |                               ',
+     |                                '',
+      ',,-,                           ':;
+           ',,| ;,,                 ,' ;;
+              ! ; !'',,,',',,,,'!  ;   ;:
+             : ;  ! !       ! ! ;  ;   :;
+             ; ;   ! !      ! !  ; ;   ;,
+            ; ;    ! !     ! !   ; ;     
+            ; ;    ! !    ! !     ; ;
+           ;,,      !,!   !,!     ;,;
+           /_I      L_I   L_I     /_I
+Yey, our little camel is sunbathing!
+-----------------------------------------------"""
+
+lion = """
+----------------------------------------------------------------------
+Switching on camera from habitat with lions...
+                                               ,w.
+                                             ,YWMMw  ,M  ,
+                        _.---.._   __..---._.'MMMMMw,wMWmW,
+                   _.-""        '''           YP"WMMMMMMMMMb,
+                .-' __.'                   .'     MMMMW^WMMMM;
+    _,        .'.-'"; `,       /`     .--""      :MMM[==MWMW^;
+ ,mM^"     ,-'.'   /   ;      ;      /   ,       MMMMb_wMW"  @\\
+,MM:.    .'.-'   .'     ;     `\    ;     `,     MMMMMMMW `"=./`-,
+WMMm__,-'.'     /      _.\      F'''-+,,   ;_,_.dMMMMMMMM[,_ / `=_}
+"^MP__.-'    ,-' _.--""   `-,   ;       \  ; ;MMMMMMMMMMW^``; __|
+           /   .'            ; ;         )  )`{  \ `"^W^`,   \  :
+          /  .'             /  (       .'  /     Ww._     `.  `"
+         /  Y,              `,  `-,=,_{   ;      MMMP`""-,  `-._.-,
+        (--, )                `,_ / `) \/"")      ^"      `-, -;"\:
+The lion is croaking!
+--------------------------------------------------------------------"""
+
+deer = """
+----------------------------------------------
+Switching on camera from habitat with deers...
+   /|       |\\
+`__\\\\       //__'
+   ||      ||
+ \__`\     |'__/
+   `_\\\\   //_'
+   _.,:---;,._
+   \_:     :_/
+     |@. .@|
+     |     |
+     ,\.-./ \\
+     ;;`-'   `---__________-----.-.
+     ;;;                         \_\\
+     ';;;                         |
+      ;    |                      ;
+       \   \     \        |      /
+        \_, \    /        \     |\\
+          |';|  |,,,,,,,,/ \    \ \_
+          |  |  |           \   /   |
+          \  \  |           |  / \  |
+           | || |           | |   | |
+           | || |           | |   | |
+           | || |           | |   | |
+           |_||_|           |_|   |_|
+          /_//_/           /_/   /_/
+Our 'Bambi' looks hungry. Let's go to feed it!
+-----------------------------------------------"""
+
+goose = """
+-----------------------------------------------------
+Switching on camera from habitat with lovely goose...
+
+                                    _
+                                ,-"" "".
+                              ,'  ____  `.
+                            ,'  ,'    `.  `._
+   (`.         _..--.._   ,'  ,'        \    \\
+  (`-.\    .-""        ""'   /          (  d _b
+ (`._  `-"" ,._             (            `-(   \\
+ <_  `     (  <`<            \              `-._\\
+  <`-       (__< <           :
+   (__        (_<_<          ;
+    `------------------------------------------
+This bird stares intently at you... (Maybe it's time to change the channel?)
+----------------------------------------------------------------------------"""
+
+bat = """
+----------------------------------------------
+Switching on camera from habitat with bats...
+_________________               _________________
+ ~-.              \  |\___/|  /              .-~
+     ~-.           \ / o o \ /           .-~
+        >           \\\\  W  //           <
+       /             /~---~\             \\
+      /_            |       |            _\\
+         ~-.        |       |        .-~
+            ;        \     /        i
+           /___      /\   /\      ___\\
+                ~-. /  \_/  \ .-~
+                   V         V
+It looks like this bat is fine.
+------------------------------------------------"""
+
+rabbit = """
+------------------------------------------------
+Switching on camera from habitat with rabbits...
+         ,
+        /|      __
+       / |   ,-~ /
+      Y :|  //  /
+      | jj /( .^
+      >-"~"-v"
+     /       Y
+    jo  o    |
+   ( ~T~     j
+    >._-' _./
+   /   "~"  |
+  Y     _,  |
+ /| ;-"~ _  l
+/ l/ ,-"~    \\
+\//\/      .- \\
+ Y        /    Y 
+ l       I     !
+ ]\      _\    /"\\
+(" ~----( ~   Y.  )
+It seems there will be more rabbits soon!
+------------------------------------------------"""
+habitat = [camel, lion, deer, goose, bat, rabbit]
+
+while True:
+    user_input = input('Which habitat # do you need? > ')
+    if user_input != 'exit':
+        user_input_conversion = int(user_input)
+        if user_input_conversion == 1:
+            print(habitat[0])
+        elif user_input_conversion == 2:
+            print(habitat[1])
+        elif user_input_conversion == 3:
+            print(habitat[2])
+        elif user_input_conversion == 4:
+            print(habitat[3])
+        elif user_input_conversion == 5:
+            print(habitat[4])
+        elif user_input_conversion == 6:
+            print(habitat[5])
+        elif user_input_conversion > 6:
+            print("-----------------------------------------------")
+            print("Please select a habitat number between 1 and 6.")
+            print("-----------------------------------------------")
+    else:
+        print("See you!")
+        break
